71902455486623123fc1a62c768d0836f757955b
Rishabh Sharma <rdmighty@gmail.com>
2019-05-27 20:25:35 +0530

    Update EntityDateOnlyMapper.ts

--- EntityDateOnlyMapper.ts
+++ EntityDateOnlyMapper.ts
@@ -1,95 +1,125 @@
-import * as moment from 'moment';
-
-Date.prototype.addTimezoneOffset = function(){
-    if(this){
-        var _mThis = moment(this);
-        return moment(this).add(_mThis.utcOffset()/60, 'hours').toDate();
-    }
-
-    return this;
-};
-
-Date.prototype.startOfTheDay = function(){
-    if(this)
-        return this.hours(0).minutes(0).seconds(0).add(this.utcOffset()/60, 'hours');    
-
-    return this;
-};
-
-moment.prototype.addTimezoneOffset = function(){
-    if(this){
-        return this.add(this.utcOffset()/60, 'hours').toDate();
-    }
-}
-
-moment.prototype.startOfTheDay = function(){
-    if(this){
-        var _moment = moment(this);
-        return _moment.hours(0).minutes(0).seconds(0).add(_moment.utcOffset()/60, 'hours').toDate();
-    }
-}
-
 import * as _ from 'underscore';
+import * as moment from 'moment';
 import { Observable } from 'rxjs';
 
-abstract class EntityDateOnlyMapper{
-    outgoing: string[];    
-    incoming: string[];
-    entity: any;
+// Declarations
+interface IEntityDateOnlyMapper{
+    props: string[];
+}
+
+abstract class EntityDateOnlyMapper implements IEntityDateOnlyMapper{
+    props: string[] ;    
 }
 
 var maps: {[key:string]: EntityDateOnlyMapper} = {};
 
-class VisualTaskDateOnlyMapper extends EntityDateOnlyMapper{
+class TaskDateOnlyMapper extends EntityDateOnlyMapper{
     constructor(){
         super();
-        this.entity = "VisualTask"
-        this.outgoing = ['startDate', 'finishDate'];
-        this.incoming = ['transition'];
-    }
+        this.props = ['startDate', 'finishDate'];
+    }    
 }
 
-maps["VisualTask"] = new VisualTaskDateOnlyMapper();
+class ProjectDateOnlyMapper extends EntityDateOnlyMapper{
+    constructor(){
+        super();
+        this.props = ['startDate', 'endDate'];
+    }    
+}
 
-export class DateOnlyApiCaller{
-    static callApi(fn: (...any) => any, args?: any[]): Observable<any>{
-        DateOnlyApiCaller.dateOnly(false, args);
+// Definitions
+maps["VisualTask"] = new TaskDateOnlyMapper();
+maps["WorkplanTask"] = maps["VisualTask"];
+maps["ProjectplanTask"] = maps["VisualTask"];
+maps["RequestplanTask"] = maps["VisualTask"];
+maps["CreateOrEditWorkplanTaskDto"] = maps["VisualTask"];
+maps["CreateorEditProjectplanTaskDto"] = maps["VisualTask"];
+maps["CreateOrEditProjectDto"] = new ProjectDateOnlyMapper();
+
+// Utility
+var utils = {
+    readValueAtNav: function(obj, nav): any{
+        if (Array.isArray(nav)) {
+            if (nav.length == 1 && obj)
+                return obj[nav[0]];
+            else if (nav.length > 1 && obj)
+                return utils.readValueAtNav(obj[nav[0]], nav.splice(1));
+            else
+                return null;
+        }
+        else
+            return utils.readValueAtNav(obj, nav.split('.'));
+    },
+    assignValueAtNav: function(obj, nav, value){
+        if (Array.isArray(nav)) {
+            if (nav.length == 1 && obj)
+                return obj[nav[0]] = value;
+            else if (nav.length > 1 && obj)
+                return utils.assignValueAtNav(obj[nav[0]], nav.splice(1), value);            
+        }
+        else
+            return utils.assignValueAtNav(obj, nav.split('.'), value);
+    },
+    objectKeysWithNav: function(obj, suffix = null, arr = null, includeOnlyMomentProp = true){
+		arr = arr || [];
+        suffix = suffix ? suffix: '';
+        
+        if(obj instanceof Object){
+            var keys = Object.keys(obj);
+            for(var i = 0; i < keys.length; i ++){
+                if(obj[keys[i]] instanceof Object && !moment.isMoment(obj[keys[i]])){
+                    utils.objectKeysWithNav(obj[keys[i]],suffix ? suffix + "." + keys[i]: keys[i], arr);            
+                }else{	
+                    if(!includeOnlyMomentProp || includeOnlyMomentProp && obj[keys[i]] instanceof moment)				
+                        arr.push(suffix ? suffix + "." + keys[i]: keys[i]);            
+                }
+            }
+        }
+        return arr;
+    }
+};
+
+// DaetOnly Apis
+export class DateOnlyApi{
+    static callApi(fnName: string, args?: any[]): Observable<any>{
+        DateOnlyApi.dateOnly(false, args);
 
         return new Observable((obs: any) => {
-            fn.apply(this, args).subscribe(result => {
-                DateOnlyApiCaller.dateOnly(true, result)
+            this[fnName].apply(this, args).subscribe(result => {
+                DateOnlyApi.dateOnly(true, [result])
                 obs.next(result);
                 obs.complete();
             });
         });
     }
 
-    static dateOnly(incoming: boolean, args?: any[]){
+    static dateOnly(incoming: boolean, args?: any | any[]){
         if(args){
-            args.forEach(item => {
-                if(item instanceof Array){
-                    DateOnlyApiCaller.dateOnly(incoming, item);
-                    return;
-                }
+            if(args instanceof Array){
+                args.forEach(item => {
+                    DateOnlyApi.dateOnly(incoming, item);
+                });
+            }else{
+                if(args instanceof Object){                    
+                    var _map = maps[args.constructor.name];
 
-                if(item instanceof Object){
-                    var _map = maps[item.constructor.name];
-        
-                    var keys = Object.keys(item);
-        
-                    _.forEach(keys, key => {
-                        if(incoming){
-                            if(_map.incoming.indexOf(key) != -1){
-                                item[key] = item[key].addTimezoneOffset();
-                            }
-                        }else{
-                            if(_map.outgoing.indexOf(key) != -1){
-                                item[key] = item[key].startOfTheDay();
-                            }
-                        }                        
-                    });
+                    if(_map){
+                        _map.props.forEach(prop => {
+                            if(incoming)
+                                utils.assignValueAtNav(args, prop, utils.readValueAtNav(args, prop).maintainUtc());
+                            else
+                                utils.assignValueAtNav(args, prop, utils.readValueAtNav(args, prop).startOfTheDay()); 
+                        });
+                    }else{
+                        var keys = Object.keys(args);
+                        
+                        _.forEach(keys, key => {
+                            if((args[key] instanceof Object) && !(args[key] instanceof moment))
+                                DateOnlyApi.dateOnly(incoming, args[key]);
+                        });                        
+                    }                                        
                 }
-            });
+            }            
         }                
-    }
+    }    
 }
